e2d7197847344ae3932d337cf65b9a0d542ee7c5
yeonha99 <rhdqn1207@naver.com>
2021-12-03 18:24:50 +0900

    feat: config

--- src/main/java/com/example/Discovery/WebSecurityConfig.java
+++ src/main/java/com/example/Discovery/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.Discovery;
 
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -12,7 +13,7 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
 
-
+@Configuration
 @EnableWebSecurity
     class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
