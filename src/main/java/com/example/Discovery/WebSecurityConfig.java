00f172972e48f7671a453035d20a0f303d124a36
yeonha99 <rhdqn1207@naver.com>
2021-12-03 18:19:20 +0900

    feat: cors 오류 허용

--- src/main/java/com/example/Discovery/WebSecurityConfig.java
+++ src/main/java/com/example/Discovery/WebSecurityConfig.java
@@ -1,9 +1,17 @@
 package com.example.Discovery;
 
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
 
 @EnableWebSecurity
     class WebSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -13,9 +21,25 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
             http.csrf().ignoringAntMatchers("/eureka/**")
                     .and()
                     .authorizeRequests()
-                    .antMatchers( "/**").permitAll();
+                    .antMatchers( "/**").permitAll()
+                    .and()
+                    .cors();
+            ;
 
             super.configure(http);
         }
+
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("HEAD", "GET", "POST", "PUT"));
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
     }
 
